0b8d458c8595f250e5aad73e9244a6a5f4ae6ed6
ALTSETING <vladislav.melnyk.kripto@gmail.com>
2026-09-03 22:30:14 +0200

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 window.PAI_SHO_CONFIG = {
-  API_URL: "https://pai-sho-server-altseting.onrender.com",
-  SOCKET_URL: "https://pai-sho-server-altseting.onrender.com"
+  API_URL: "https://pai-ho.onrender.com",
+  SOCKET_URL: "https://pai-ho.onrender.com"
 };
